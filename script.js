717722fb98caa1551a9ea380929935b3cee51e5e
ThildeOn02 <57523448+ThildeOn02@users.noreply.github.com>
2020-02-27 12:14:17 +0100

    Efterliknat en sida och lagt till lite saker

---
+++ script.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector(' .navLinks')
+const burger = document.querySelector(' .burger')
+
+function toggleMeny() {
+nav.classList.toggle('navActive')
+eleven.classList.toggle('toggle')
+}
+
+burger.addEventListener('click', toggleMeny)
