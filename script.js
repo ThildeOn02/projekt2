ebf1938c6583307ddfd8932fdf8525f00a12bab9
ThildeOn02 <57523448+ThildeOn02@users.noreply.github.com>
2020-03-19 12:23:37 +0100

    ändringar

--- script.js
+++ script.js
@@ -9,5 +9,9 @@ eleven.classList.toggle('toggle')
 burger.addEventListener('click', toggleMeny)
 
 
+function toggleMeny() {
+    nav.classList.toggle('searchActive')
+    eleven.classList.toggle('toggle')
+    }
 
 search.addEventListener('click', toggleMeny )
